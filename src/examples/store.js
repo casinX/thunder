df6434fd1997f94c18e44d5e641d26d8d955ab34
Антон Чащин <anton.casin@gmail.com>
2018-01-05 04:28:10 +0300

    fix example store

--- src/examples/store.js
+++ src/examples/store.js
@@ -4,17 +4,17 @@ import { Container } from 'lib';
 const user = new Container({
     age: 0,
     repos: [],
-});
+})
 
-user.action('increment', () => {
+.action('increment', () => {
     user.data.age ++;
-});
+})
 
-user.action('decrement', () => {
+.action('decrement', () => {
     user.data.age --;
-});
+})
 
-user.async('loadGit', async () => {
+.async('loadGit', async () => {
     const response = await user.axios.get('https://api.github.com/users/casinX/repos');
     user.data.repos = response.data;
 });
